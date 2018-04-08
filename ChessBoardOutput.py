1a263dcfec46a3cfc4ce7c106c8b0acdb074b414
Ram Moore <rammoore112358@gmail.com>
2018-04-08 03:45:04 -0500

    Very Very Version 0.1 It has minimum functionality but we'll see where it goes. Also how should I manage a todo list?

--- ChessBoardOutput.py
+++ ChessBoardOutput.py
@@ -6,14 +6,14 @@ def printOneBoard(board,n,m):
         sys.stdout.write("%2s" % (i+1))
     print("")
     for i in range (0,2*n+1):
-        if (i%2 == 1):
+        if i%2 == 1:
             sys.stdout.write("%s" % (i/2+1))
         else:
             sys.stdout.write(" ")
         for j in range (0,2*m+1):
-            if (i%2 == 0):
+            if i%2 == 0:
                 sys.stdout.write("-")
-            elif (j % 2 == 0):
+            elif j % 2 == 0:
                 sys.stdout.write("|")
             else:
                 sys.stdout.write(board[i/2][j/2])
@@ -26,27 +26,30 @@ def printNineBoards(board,n,m):
     print("")
     for _ in range (0,3):
         for i in range (0,2*n+1):
-            if (i%2 == 1):
+            if i%2 == 1:
                 sys.stdout.write("%s" % (i/2+1))
             else:
                 sys.stdout.write(" ")
             for _ in range (0,3):
                 for j in range (0,2*m+1):
-                    if (i%2 == 0):
+                    if i%2 == 0:
                         sys.stdout.write("-")
-                    elif (j % 2 == 0):
+                    elif j % 2 == 0:
                         sys.stdout.write("|")
                     else:
                         sys.stdout.write(board[i/2][j/2])
             print("")
-def placePiece(piece,n,m)
 
-
-n, m = raw_input("How big do you want the board (two numbers:n m): ").split(' ')
+n, m = raw_input("How big do you want the board (two numbers:n m): ").split()
 n=int(n)
 m=int(m)
 
-board = [[' '] * m] * n
+board = [[' ' for _ in range(m)] for _ in range(n)]
 
-printOneBoard(board,n,m)
-printNineBoards(board,n,m)
+while True:
+    printOneBoard(board,n,m)
+    printNineBoards(board,n,m)
+    piece, c, r = raw_input("Enter the piece followed by the colum and row (piece c r):").split()
+    r=int(r) - 1
+    c=int(c) - 1
+    board[r][c] = piece
