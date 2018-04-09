4c38c93a7510bbe95a2cd7451632bb9689379f02
Ram Moore <rammoore112358@gmail.com>
2018-04-09 18:17:45 -0500

    Moved to using multiple files Added out of bounds correction and it detects kings (I) and knights (K) Pieces can only be placed over and empty spot or an ownership symbol

---
+++ ChessBoardFunctions.py
@@ -0,0 +1,82 @@
+import sys
+
+def printOneBoard(board,n,m):
+    sys.stdout.write(" ")
+    for i in range (0,m):
+        sys.stdout.write("%2s" % (i+1))
+    print("")
+    for i in range (0,2*n+1):
+        if i%2 == 1:
+            sys.stdout.write("%s" % (i/2+1))
+        else:
+            sys.stdout.write(" ")
+        for j in range (0,2*m+1):
+            if i%2 == 0:
+                sys.stdout.write("-")
+            elif j % 2 == 0:
+                sys.stdout.write("|")
+            else:
+                sys.stdout.write(board[i/2][j/2])
+        print("")
+
+def printNineBoards(board,n,m):
+    for _ in range (0,3):
+        sys.stdout.write(" ")
+        for i in range (0,m):
+            sys.stdout.write("%2s" % (i+1))
+    print("")
+    for _ in range (0,3):
+        for i in range (0,2*n+1):
+            if i%2 == 1:
+                sys.stdout.write("%s" % (i/2+1))
+            else:
+                sys.stdout.write(" ")
+            for _ in range (0,3):
+                for j in range (0,2*m+1):
+                    if i%2 == 0:
+                        sys.stdout.write("-")
+                    elif j % 2 == 0:
+                        sys.stdout.write("|")
+                    else:
+                        sys.stdout.write(board[i/2][j/2])
+            print("")
+
+def limitPiece(n,m,r,c):
+    if r >= n or r < 0:
+        r = r%n
+    if c >= m or c < 0:
+        c = c%m
+    return r , c
+
+# checks if it's a chess piece and shows ownership markers, if not just places the peice
+def placeChess(board,n,m,piece,r,c):
+    if piece == "K":
+        board = placePiece(board,n,m,piece,r,c)
+        board = placePiece(board,n,m,"~",r+1,c+1)
+        board = placePiece(board,n,m,"~",r+1,c-1)
+        board = placePiece(board,n,m,"~",r+1,c  )
+        board = placePiece(board,n,m,"~",r-1,c+1)
+        board = placePiece(board,n,m,"~",r-1,c-1)
+        board = placePiece(board,n,m,"~",r-1,c  )
+        board = placePiece(board,n,m,"~",r  ,c+1)
+        board = placePiece(board,n,m,"~",r  ,c-1)
+    elif piece == "I":
+        board = placePiece(board,n,m,piece,r,c)
+        board = placePiece(board,n,m,"~",r+2,c+1)
+        board = placePiece(board,n,m,"~",r+2,c-1)
+        board = placePiece(board,n,m,"~",r-2,c+1)
+        board = placePiece(board,n,m,"~",r-2,c-1)
+        board = placePiece(board,n,m,"~",r+1,c+2)
+        board = placePiece(board,n,m,"~",r-1,c+2)
+        board = placePiece(board,n,m,"~",r+1,c-2)
+        board = placePiece(board,n,m,"~",r-1,c-2)
+    else:
+        board = placePiece(board,n,m,piece,r,c)
+    return board;
+
+# uses limitPiece and then places the piece so long as it isn't filled
+def placePiece(board,n,m,piece,r,c):
+    r , c = limitPiece(n,m,r,c)
+    if board[r][c] == " ":
+        board[r][c] = piece
+    return board;
--- ChessBoardOutput.py
+++ ChessBoardOutput.py
@@ -1,44 +1,4 @@
-import sys
-
-def printOneBoard(board,n,m):
-    sys.stdout.write(" ")
-    for i in range (0,m):
-        sys.stdout.write("%2s" % (i+1))
-    print("")
-    for i in range (0,2*n+1):
-        if i%2 == 1:
-            sys.stdout.write("%s" % (i/2+1))
-        else:
-            sys.stdout.write(" ")
-        for j in range (0,2*m+1):
-            if i%2 == 0:
-                sys.stdout.write("-")
-            elif j % 2 == 0:
-                sys.stdout.write("|")
-            else:
-                sys.stdout.write(board[i/2][j/2])
-        print("")
-def printNineBoards(board,n,m):
-    for _ in range (0,3):
-        sys.stdout.write(" ")
-        for i in range (0,m):
-            sys.stdout.write("%2s" % (i+1))
-    print("")
-    for _ in range (0,3):
-        for i in range (0,2*n+1):
-            if i%2 == 1:
-                sys.stdout.write("%s" % (i/2+1))
-            else:
-                sys.stdout.write(" ")
-            for _ in range (0,3):
-                for j in range (0,2*m+1):
-                    if i%2 == 0:
-                        sys.stdout.write("-")
-                    elif j % 2 == 0:
-                        sys.stdout.write("|")
-                    else:
-                        sys.stdout.write(board[i/2][j/2])
-            print("")
+import ChessBoardFunctions
 
 n, m = raw_input("How big do you want the board (two numbers:n m): ").split()
 n=int(n)
@@ -47,9 +7,9 @@ m=int(m)
 board = [[' ' for _ in range(m)] for _ in range(n)]
 
 while True:
-    printOneBoard(board,n,m)
-    printNineBoards(board,n,m)
+    ChessBoardFunctions.printOneBoard(board,n,m)
+    ChessBoardFunctions.printNineBoards(board,n,m)
     piece, c, r = raw_input("Enter the piece followed by the colum and row (piece c r):").split()
     r=int(r) - 1
     c=int(c) - 1
-    board[r][c] = piece
+    board = ChessBoardFunctions.placeChess(board,n,m,piece,r,c)
